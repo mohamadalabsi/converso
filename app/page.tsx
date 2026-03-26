ed9b8ddc271a0e9b6b3888c4496809b9575b67c4
mohamadalabsi <absimoda1@gmail.com>
2026-03-26 02:34:28 +0100

    updating project files

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import {Button} from "@/components/ui/button";
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div><h1 >welcome</h1>
+    <Button>
+      lets get started!
+    </Button>
+    </div>
   )
 }
 
