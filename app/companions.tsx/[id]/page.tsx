6a5c7287cd23863fe0311eb0d34e1c48b6ddf4d9
mohamadalabsi <absimoda1@gmail.com>
2026-03-26 19:53:58 +0100

    creating routes

---
+++ app/companions.tsx/[id]/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+import React from 'react'
+
+const CompanionSession = () => {
+  return (
+    <div>Companion Session</div>
+  )
+}
+
+export default CompanionSession
---
+++ app/companions.tsx/page.tsx
@@ -0,0 +1,11 @@
+
+
+
+export default function CompanionsLibrary() {
+  return (
+    <div>
+      <h1>Companions Library</h1>
+      {/* Add your companions library components or content here */}
+    </div>
+  )
+}
